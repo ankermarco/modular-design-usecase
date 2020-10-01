37deb4258f359609f5762b7c4eeed892b8ab7eeb
Ke.Ma <ke.ma@progressive-tech.co.uk>
2020-10-01 22:56:43 +0100

    Create FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Ke Ma on 01/10/2020.
+//  Copyright © 2020 Ke. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
